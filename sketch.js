f00dac9e9dd1428f8ded1e3f1d3c0e9960623bcc
igorternyuk <mr.ternyuk@mail.ru>
2018-05-29 20:43:02 +0300

    first commit

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var canvasWidth = 400;
+var canvasHeight = 400;
+
+function setup() {
+    createCanvas(canvasWidth, canvasHeight);
+    frameRate(10);
+}
+
+
+//main loop
+function draw() {
+	background(0);
+}
+
+
+
